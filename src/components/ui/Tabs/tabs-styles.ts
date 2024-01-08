6d9f9835309b7f1246f5023aaa4dbdc32dd32f0e
Abhinav Moghe <abhinavmoghe01@gmail.com>
2024-01-08 11:28:17 +0530

    removed comments

--- src/components/ui/Tabs/tabs-styles.ts
+++ src/components/ui/Tabs/tabs-styles.ts
@@ -1,7 +1,6 @@
 import styled, { css } from "styled-components"
 import { BaseLink, BaseSpan, BaseUnorderedList } from "../../../base-styles"
 
-// TO-DO: Check if ul from drop-down can be re-used here
 export const TabList = styled.ul`
   ${BaseUnorderedList}
   height: 4rem;
@@ -21,7 +20,6 @@ export const Tab = styled.li<{ selected: boolean }>`
   padding: 0.5rem;
 `
 
-// TO-DO: Check if a from drop-down can be re-used here
 export const TabContent = styled.a`
   ${BaseLink}
   padding: 0 1rem;
